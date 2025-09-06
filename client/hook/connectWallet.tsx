359b970edc064e520eb5ae0a732897fb711bcdcb
Jnyandeep <thokchom.jnyandeep@gmail.com>
2025-09-06 16:40:07 +0530

    feat: wallet connections and userData

--- client/hook/connectWallet.tsx
+++ client/hook/connectWallet.tsx
@@ -1,20 +1,62 @@
-import { useState } from "react";
-import { connect, disconnect, isConnected } from "@stacks/connect";
+import { getStxBalance } from "@/lib/stx-utils";
+import {
+  AppConfig,
+  showConnect,
+  UserSession,
+  type UserData,
+} from "@stacks/connect";
+import { useEffect, useState } from "react";
 
-async function connecteWallet() {
-  if (isConnected()) {
-    disconnect();
-    return;
+const appDetails = {
+  name: "Boo",
+  icon: "../public/logo-orng.svg",
+};
+
+const appConfig = new AppConfig(["store_write"]);
+const userSession = new UserSession({ appConfig });
+
+export function useStacks() {
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [stxBalance, setStxBalance] = useState(0);
+
+  function connectWallet() {
+    showConnect({
+      appDetails,
+      onFinish: () => {
+        window.location.reload();
+      },
+      userSession,
+    });
   }
-  const response = await connect();
 
-  // const address = await a
-}
+  function disconnectWallet() {
+    userSession.signUserOut();
+    setUserData(null);
+  }
 
-export default function ConnectWallet() {
-  const [address, setAddress] = useState("");
+  useEffect(() => {
+    if (userSession.isSignInPending()) {
+      userSession.handlePendingSignIn().then((userData) => {
+        setUserData(userData);
+      });
+    } else if (userSession.isUserSignedIn()) {
+      setUserData(userSession.loadUserData());
+    }
+  }, []);
 
-  // const handleWalletConnect = async () => {
-  //     const addr = await
-  // }
+  useEffect(() => {
+    if (userData) {
+      const address = userData.profile.stxAddress.testnet;
+      getStxBalance(address).then((balance) => {
+        setStxBalance(balance);
+      });
+    }
+  }, [userData]);
+
+  return {
+    userData,
+    stxBalance,
+    connectWallet,
+    disconnectWallet,
+  };
 }
